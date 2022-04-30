911f237533bcaefd95adb59347b002c82b87ae4f
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-04-30 11:23:51 +0300

    add filemanager;

--- src/main/java/com/bondarenko/filemanager/FileManager.java
+++ src/main/java/com/bondarenko/filemanager/FileManager.java
@@ -8,16 +8,16 @@ import java.io.IOException;
 public class FileManager {
 
     public static int countFiles(String path) {
-        int count = 0;
+        int countFiles = 0;
         File dir = new File(path);
         for (File file : dir.listFiles()) {
             if (file.isDirectory()) {
-                count += countFiles(file.getAbsolutePath());
+                countFiles += countFiles(file.getAbsolutePath());
             } else {
-                count++;
+                countFiles++;
             }
         }
-        return count;
+        return countFiles;
     }
 
     public static int countDirs(String path) {
@@ -35,20 +35,12 @@ public class FileManager {
     public static void copy(String from, String to) {
         File fileToCopy = new File(from);
         File fileCopied = new File(to);
-        copyFileOrDirs(fileToCopy, fileCopied);
-    }
-
-
-    // Параметр from - путь к файлу или папке, параметр to - путь к папке куда будет производиться перемищение.
-    public void move(String from, String to) {
-        File pathFrom = new File(from);
-        File pathTo = new File(to, pathFrom.getName());
-        pathFrom.renameTo(pathTo);
+        copyFileContent(fileToCopy, fileCopied);
     }
 
     public void clean(File file) {
         File[] list = file.listFiles();
-        checkNotNull(list);
+        checkFileIsExisting(list);
         for (File files : list) {
             if (files.isDirectory()) {
                 File newDir = new File(file + File.separator + files.getName());
@@ -59,42 +51,40 @@ public class FileManager {
         file.delete();
     }
 
+    public void move(String from, String to) {
+        File pathFrom = new File(from);
+        File pathTo = new File(to, pathFrom.getName());
+        pathFrom.renameTo(pathTo);
+    }
 
-    private static void copyFileOrDirs(File from, File to) {
+    private static void copyFileContent(File from, File to) {
         if (from.isDirectory()) {
             if (!to.exists()) {
                 to.mkdir();
             }
             String[] files = from.list();
             for (int i = 0; i < files.length; i++) {
-                copyFileOrDirs(new File(from, files[i]), new File(to, files[i]));
+                copyFileContent(new File(from, files[i]), new File(to, files[i]));
             }
         } else {
             try (FileInputStream fileInputStream = new FileInputStream(from);
                  FileOutputStream fileOutputStream = new FileOutputStream(to)) {
-                int length = (int) from.length();
-                byte[] buffer = new byte[length];
+                int length;
+                byte[] buffer = new byte[1024];
                 while ((length = fileInputStream.read(buffer)) > 0) {
                     fileOutputStream.write(buffer, 0, length);
                 }
             } catch (IOException exception) {
-                throw new RuntimeException("", exception);// дописать
+                throw new RuntimeException("Cant read or write FileContent", exception);
             }
         }
     }
 
-    private static void checkNotNull(File[] file) {
+    private static void checkFileIsExisting(File[] file) {
         if (file == null) {
-            throw new NullPointerException("Don't have permission to use file or not exist file");
+            throw new NullPointerException("File is not exist");
         }
     }
 }
 
-//принимает путь к папке, возвращает количество файлов в папке и всех подпапках по пути
-//public static int countDirs(String path) - принимает путь к папке,
-// возвращает количество папок в папке и всех подпапках по пути
-
-//public static void copy(String from, String to) - метод по копированию папок и файлов.
-
-//метод по перемещению папок и файлов.
 
